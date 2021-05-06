3419dc8a62a9d8fac5b0dbec8132d28307ded86e
bumsun <bumsun@yandex.ru>
2021-05-06 19:13:19 +0300

    first commit

---
+++ UpMobSDKEvents/src/main/java/com/example/upmobsdkevents/UpMobSDK.java
@@ -0,0 +1,11 @@
+package com.example.upmobsdkevents;
+
+public class UpMobSDK {
+    private String test = "Test value";
+    public UpMobSDK() {
+    }
+
+    public String getTest() {
+        return test;
+    }
+}
