1d9abaf269113cb03f27a1ebe267fbe04d1cf879
bumsun <bumsun@yandex.ru>
2021-05-06 20:27:44 +0300

    first commit

---
+++ UpMobEvents/src/main/java/com/upmob/upmobevents/UpMob.java
@@ -0,0 +1,12 @@
+package com.upmob.upmobevents;
+
+public class UpMob {
+    private String test = "test value";
+
+    public UpMob() {
+    }
+
+    public String getTest() {
+        return test;
+    }
+}
