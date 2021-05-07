8458a51127b8fa69c5c89bc791f9b9766ca2b6db
bumsun <bumsun@yandex.ru>
2021-05-07 22:21:08 +0300

    first commit

--- app/src/main/java/com/upmob/upmobsdk/MainActivity.java
+++ app/src/main/java/com/upmob/upmobsdk/MainActivity.java
@@ -3,12 +3,26 @@ package com.upmob.upmobsdk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.upmob.upmobevents.UpMob;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        UpMob.init(getApplicationContext(),"12345");
+
+        Handler handler = new Handler(getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(null);
+                UpMob.sendEvent("333333");
+            }
+        },2000);
+
     }
 }
